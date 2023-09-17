a2177aa8588fe6b7915b153cdee35f449507c8fc
Lantena <772456648@qq.com>
2023-09-17 11:07:30 +0100

    Exercise 5.

---
+++ exercise5.js
@@ -0,0 +1,16 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+
+    const uncompleted = json.reduce( (accumulator, todo) => {
+        if (!todo.completed) {
+            accumulator.push({ userId: todo.userId, title: todo.title });
+          }
+        return accumulator
+    },[])
+
+    console.log(uncompleted)
+})
+.catch(function(err) { 
+  console.log(err);
+});
